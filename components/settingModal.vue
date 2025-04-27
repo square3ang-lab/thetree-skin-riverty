d32fa976eea80967bfab00a9e7325f00797c266d
square3ang <square3ang@gmail.com>
2025-04-27 14:08:56 +0900

    critical mistake

--- components/settingModal.vue
+++ components/settingModal.vue
@@ -33,7 +33,7 @@ export default {
     },
     computed: {
         sidebar() {
-            return isMobile ? "bottom" : "right";
+            return isMobile ? "footer" : "right";
         }
     }
 }
